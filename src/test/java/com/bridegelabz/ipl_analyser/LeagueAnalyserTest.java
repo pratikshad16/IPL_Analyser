9cc295f1153f46dd46181dfdc173c8e42a8f20c8
pratikshad16 <dhagepratiksha16@gmail.com>
2020-09-13 15:54:32 +0530

    Added: Ability to find top batting averages of cricketers

--- src/test/java/com/bridegelabz/ipl_analyser/LeagueAnalyserTest.java
+++ src/test/java/com/bridegelabz/ipl_analyser/LeagueAnalyserTest.java
@@ -1,6 +1,8 @@
 package com.bridegelabz.ipl_analyser;
 import com.bridgelabz.ipl_analyser.LeagueAnalyser;
 import com.bridgelabz.ipl_analyser.LeagueAnalyserException;
+import com.bridgelabz.ipl_analyser.LeagueRunsCSV;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,6 +26,18 @@ public class LeagueAnalyserTest {
             LeagueAnalyser leagueAnalyser = new LeagueAnalyser();
             int records = leagueAnalyser.loadData(LeagueAnalyser.Player.WKTS, WKTS_SHEET);
             Assert.assertEquals(99, records);
+        } catch (LeagueAnalyserException e) {
+            Assert.assertEquals(LeagueAnalyserException.ExceptionType.FILE_TYPE_OR_DELIMITER_OR_HEADER_PROBLEM, e.type);
+        }
+    }
+    @Test
+    public void givenFile_SortWithRespectToAverage_ReturnHighestBattingAvg()  {
+        try {
+            LeagueAnalyser leagueAnalyser = new LeagueAnalyser();
+            leagueAnalyser.loadData(LeagueAnalyser.Player.RUNS, RUNS_SHEET);
+            String sortedData = leagueAnalyser.sortData("AVG");
+            LeagueRunsCSV[] leagueRunsCSVS = new Gson().fromJson(sortedData, LeagueRunsCSV[].class);
+            Assert.assertEquals("Ben Cutting", leagueRunsCSVS[0].name);
         } catch (LeagueAnalyserException e) {
             Assert.assertEquals(LeagueAnalyserException.ExceptionType.FILE_TYPE_OR_DELIMITER_OR_HEADER_PROBLEM, e.type);
         }
